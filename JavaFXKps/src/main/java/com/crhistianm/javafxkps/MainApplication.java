d332bfce19f0cf47100833f99d99c247fcc87820
CrhistianMRe <came.29meca@gmail.com>
2025-05-09 11:18:23 +0800

    Add working onAction method for selecting course comboBox

--- JavaFXKps/src/main/java/com/crhistianm/javafxkps/MainApplication.java
+++ JavaFXKps/src/main/java/com/crhistianm/javafxkps/MainApplication.java
@@ -14,8 +14,8 @@ import com.crhistianm.javafxkps.model.Account;
 public class MainApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/login-view.fxml"));
-        //FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/com/crhistianm/javafxkps/view/steacher-view.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/com/crhistianm/javafxkps/view/steacher-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),  800, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- JavaFXKps/src/main/java/com/crhistianm/javafxkps/controller/TeacherController.java
+++ JavaFXKps/src/main/java/com/crhistianm/javafxkps/controller/TeacherController.java
@@ -1,10 +1,23 @@
 package com.crhistianm.javafxkps.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 public class TeacherController {
+
+    @FXML
+    public ComboBox comboCourse;
+
+    @FXML
+    public TextField txtSearch;
+    
+    @FXML
+    public void handleComboSelection(){
+        System.out.println("Course selected");
+
+    }
+
 
 }
