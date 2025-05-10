e6c0b6c7df505f97cd963b24699cebdc3c563eba
CrhistianMRe <came.29meca@gmail.com>
2025-05-10 14:53:45 +0800

    Add search sql

---
+++ sql/SearchGradePerAccount.sql
@@ -0,0 +1,5 @@
+SELECT account.Account_Number, student.Name, student.Surname, grade.Total_Grade, grade.Grade_Date, grade.ID
+FROM grade INNER JOIN student ON grade.Student_ID_FK = student.ID 
+    INNER JOIN subject ON grade.Subject_ID_FK = subject.ID
+    INNER JOIN account ON student.Account_ID_FK = account.ID
+    WHERE subject.Name="MongoDB" && account.Account_Number LIKE '%54893%'; 
